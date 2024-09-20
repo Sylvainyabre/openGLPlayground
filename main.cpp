d3649171d005fb948c6a2864c943ca5d8b3ee0f9
Sylvain Yabre <sylvainyabre7@gmail.com>
2024-09-20 11:45:27 -0700

    glfw header added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 using namespace std;
+
+void createObj(){
+    unsigned int windowWidth = 800;
+    unsigned int windowHeight = 600
+
+    //create object
+    unsigned int objectId = 0;
+    glGenObject(1,&objectId);
+
+    //bind object to context
+    glBindObject(GL_WINDOW_TARGET,objectId);
+    //set options of object currently bound to GL_WINDOW_TARGET
+    glSetObjectOption(GL_WINDOW_TARGET,GL_OPTION_WINDOW_WIDTH,windowWidth);
+    glSetObjectOption(GL_WINDOW_TARGET,GL_OPTION_WINDOW_WIDTH,windowHeight);
+
+    //Set context back to default
+    glBindObject(GL_WINDOW_TARGET,0)
+
+
+
+}
 
 int  main(){
     cout<<"Hello world \n";
